1d73b7cc8ed19fb2b4334704d40d85962ee6e554
rgulati2 <rahulgul@usc.edu>
2025-02-19 11:55:12 -0800

    updated large, tiny model config

--- submit_simple.sh
+++ submit_simple.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --time=23:00:00
+#SBATCH --time=47:00:00
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
-#SBATCH --job-name="h_test11a"
-#SBATCH --output="h_test11a.%j.out"
+#SBATCH --job-name="h_t11i"
+#SBATCH --output="h_test11i.%j.out"
 #SBATCH --account=garikipa_1359
 #SBATCH --export=ALL
 #SBATCH --mem=32G
@@ -18,4 +18,4 @@ module list
 nvidia-smi
 source /project/garikipa_1359/rahulgul/spatiotemporal/MPP/environment/bin/activate
 
-python train_basic1.py --run_name "h_test11a" --config "basic_config"  --yaml_config "./config/mpp_avit_b_config.yaml"  
+python train_basic1.py --run_name "h_test11i" --config "basic_config"  --yaml_config "./config/mpp_avit_L_config.yaml"  
