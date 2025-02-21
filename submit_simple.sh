da9e7a2cf2a5b4d2eba2b27980ef6ebc5f7c0bc6
rgulati2 <rahulgul@usc.edu>
2025-02-21 11:41:17 -0800

    Run elasticity shear

--- submit_simple.sh
+++ submit_simple.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
-#SBATCH --job-name="h_t11i"
-#SBATCH --output="h_test11i.%j.out"
+#SBATCH --job-name="el_t14a"
+#SBATCH --output="el_test14a.%j.out"
 #SBATCH --account=garikipa_1359
 #SBATCH --export=ALL
 #SBATCH --mem=32G
@@ -18,4 +18,4 @@ module list
 nvidia-smi
 source /project/garikipa_1359/rahulgul/spatiotemporal/MPP/environment/bin/activate
 
-python train_basic1.py --run_name "h_test11i" --config "basic_config"  --yaml_config "./config/mpp_avit_b_config.yaml"  
+python train_basic1.py --run_name "el_test14a" --config "basic_config"  --yaml_config "./config/mpp_avit_b_config.yaml"  
