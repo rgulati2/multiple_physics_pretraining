72832a64d2a9875a4a25e46d0d4f89188e73223e
rgulati2 <rahulgul@usc.edu>
2025-06-20 13:43:48 -0700

    accuracy study context 16 finetuning

--- submit_simple.sh
+++ submit_simple.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
-#SBATCH --job-name="4a"
-#SBATCH --output="4a.%j.out"
+#SBATCH --job-name="1_cntxt16_fine"
+#SBATCH --output="1_cntxt16_fine.%j.out"
 #SBATCH --account=garikipa_1359
 #SBATCH --export=ALL
 #SBATCH --mem=32G
@@ -14,4 +14,4 @@
 module list
 nvidia-smi
 source /project/garikipa_1359/rahulgul/spatiotemporal/2025/softwareInstall//pythonPath/bin/activate
-python train_basic1.py --run_name "4a" --config "finetune"  --yaml_config "./config/mpp_avit_b_config.yaml"  
+python train_basic1.py --run_name "1_cntxt16_fine" --config "finetune"  --yaml_config "./config/mpp_avit_b_config.yaml"  
